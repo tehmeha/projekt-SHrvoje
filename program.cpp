174bf82bd6dd0e2f0d963dfc14b82eb1902c3ef9
SHrvoje <hstupar1@gmail.com>
2017-11-02 11:23:49 +0100

    prvi dio

--- program.cpp
+++ program.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
-using namespacestd;
+using namespace std;
 int main ()
 {
-
+    cout<<"1.Zbrajanje";
+    cout<<"2.Oduzimanje";
+    cout<<"3.Mnozenje";
+    cout<<"4.Dijeljenje";
+    cout<<"5.Korjenovanje";
+    cout<<"6.Potenciranje";
+    cout<<"7.Logoritmiranje";
+    cout<<"8.Trigonometrijske funkcije";
+    cout<<"Odaberite broj sa izbornika: ";
+    int izbor;
 }
