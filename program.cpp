66abc8ae43c3dcc91fb92ed057803fd346c9190f
SHrvoje <hstupar1@gmail.com>
2017-11-16 10:23:42 +0100

    drugi dio

--- program.cpp
+++ program.cpp
@@ -2,14 +2,39 @@
 using namespace std;
 int main ()
 {
-    cout<<"1.Zbrajanje";
-    cout<<"2.Oduzimanje";
-    cout<<"3.Mnozenje";
-    cout<<"4.Dijeljenje";
-    cout<<"5.Korjenovanje";
-    cout<<"6.Potenciranje";
-    cout<<"7.Logoritmiranje";
-    cout<<"8.Trigonometrijske funkcije";
-    cout<<"Odaberite broj sa izbornika: ";
+    cout<<"1.Zbrajanje:"<<endl;
+    cout<<"2.Oduzimanje:"<<endl;
+    cout<<"3.Mnozenje:"<<endl;
+    cout<<"4.Dijeljenje:"<<endl;
+    cout<<"5.Korjenovanje:"<<endl;
+    cout<<"6.Potenciranje:"<<endl;
+    cout<<"7.Logoritmiranje:"<<endl;
+    cout<<"8.Trigonometrijske funkcije:"<<endl;
+    cout<<endl;
+    cout<<"Odaberite broj sa izbornika:"<<endl;
     int izbor;
+    cin>> izbor;
+    if(izbor==1)
+    {
+        double a,b;
+        cout<<"Unesite prvi broj: "<<endl;
+        cin>>a;
+        cout<<"Unesite drugi broj:"<<endl;
+        cin>>b;
+        cout<< a + b;
+        return 0;
+
+    }
+    else if(izbor==2)
+    {
+        double a,b;
+        cout<<"Unesite prvi broj: "<<endl;
+        cin>>a;
+        cout<<"Unesite drugi broj:"<<endl;
+        cin>>b;
+        cout<< a - b;
+
+    }
 }
+
+
