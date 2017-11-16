5ce1268fc929248c6ab8eff4d3472a231fa545a2
SHrvoje <hstupar1@gmail.com>
2017-11-16 11:26:01 +0100

    sd

--- program.cpp
+++ program.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
+int zbroj(int a, int b)
+{
+    return a+b;
+}
+int razlika(a-b);
+int umnozak(a*b);
+int kolicnik(a/b);
 int main ()
 {
     cout<<"1.Zbrajanje:"<<endl;
@@ -21,8 +29,7 @@ int main ()
         cin>>a;
         cout<<"Unesite drugi broj:"<<endl;
         cin>>b;
-        cout<< a + b;
-        return 0;
+        cout<<zbroj(a,b);
 
     }
     else if(izbor==2)
@@ -32,9 +39,67 @@ int main ()
         cin>>a;
         cout<<"Unesite drugi broj:"<<endl;
         cin>>b;
-        cout<< a - b;
+        cout<<razlika(a,b)
 
     }
+    else if(izbor==3)
+    {
+        double a,b;
+        cout<<"Unesite prvi broj: "<<endl;
+        cin>>a;
+        cout<<"Unesite drugi broj:"<<endl;
+        cin>>b;
+        cout<<umnozak(a,b);
+    }
+    else if(izbor==4)
+    {
+        double a,b;
+        cout<<"Unesite prvi broj: "<<endl;
+        cin>>a;
+        cout<<"Unesite drugi broj:"<<endl;
+        cin>>b;
+        cout<< kolicnik(a,b);
+    }
+      else if(izbor==5)
+    {
+        double a;
+        cout<<"Unesite prvi broj: "<<endl;
+        cin>>a;
+        cout<<  sqrt (a);
+    }
+     else if(izbor==6)
+    {
+        double a,b;
+        cout<<"Unesite prvi broj: "<<endl;
+        cin>>a;
+        cout<<"Unesite potenciju:"<<endl;
+        cin>>b;
+        cout<<pow(a,b);
+    }
+     else if(izbor==7)
+    {
+        double a,b;
+        cout<<"Unesite bazu: "<<endl;
+        cin>>a;
+        cout<<"Unesite argument:"<<endl;
+        cin>>b;
+        cout<<log10(a)/log10(b);
+    }
+     else if(izbor==8)
+    {
+        double a,b,c;
+        cout<<"Unesite argument sinusa : "<<endl;
+        cin>>a;
+        cout << "sinsu je: " << sin(a*3.141492654/180);
+        cout<<"Unesite argument cosinusa : "<<endl;
+        cin>>b;
+        cout <<"cosinus je:"<< cos(b*3.141492654/180);
+        cout<<"Unesite argument tangesa : "<<endl;
+        cin>>c;
+        cout<<"tanges je:"<< tan(c*3.141492654/180);
+        cout<<"cotanges je:"<< 1/tan(c*3.141492654/180);
+    }
+
 }
 
 
